c4c4644a3df2db7fbdb02670402a5bd4d42261ef
Erin DeVine <erinj291@gmail.com>
2024-09-12 18:09:06 -0400

    added in DB_URL for deploy

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,24 @@
 const Sequelize = require('sequelize');
-require ('dotenv').config();
+require('dotenv').config();
 
 let sequelize;
 
 try {
-  
-  sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
+  sequelize = process.env.DB_URL 
+    ? new Sequelize(process.env.DB_URL, { dialect: 'postgres' }) 
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
           host: 'localhost',
           dialect: 'postgres',
-      //    port: 3456,
         }
-  );
-  console.log("DB Connected ....")
+      );
+
+  console.log("DB Connected ....");
 } catch (error) {
   console.log("Err: ", error);
 }
-
-
-
-
 
 module.exports = sequelize;
